b88b54ba8abae83f0b318a107edd575552e1821e
Ethan W. <160505433+etw07@users.noreply.github.com>
2024-03-27 12:28:58 +0000

    final submission

--- main.py
+++ main.py
@@ -4,17 +4,37 @@ import time
 
 # Part 1
 def clock(n):
-    # Your code here
-    pass
+  # Your code here
+  """returns the current time for n seconds"""
+  for i in range(n):
+    now = datetime.now()
+    print(now.strftime('%H:%M:%S'),end = "\r")
+    time.sleep(1)
+  pass
 
 
 # Part 2
 def lcm(a, b):
-    # Your code here
-    pass
+  # Your code here
+  """returns the lowest common multiple of a and b"""
+  a_ori = a
+  b_ori = b
+  while a != b:
+    if a < b:
+      a += a_ori
+    else:
+      b += b_ori
+  return a
+  pass
 
 
 # Part 3
 def gcf(a, b):
-    # Your code here
-    pass
+  # Your code here
+  """returns the greatest common factor of a and b"""
+  while b != 0:
+    r = a%b
+    a = b
+    b = r
+  return a
+  pass
